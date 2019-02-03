953321dede8f241a5036b4890fb674c915a0956a
Zachary Lehmann <zlehmann@gmail.com>
2019-02-03 13:48:55 +0000

    Automatically backed up by Learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -9,9 +9,7 @@ module SongsHelper
 
   def display_artist(song)
     if song.artist.nil?
-      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
-    else
-      hidden_field_tag "song[artist_id]", song.artist_id
+      link_to "Add Artist", edit_song_path(song)
     end
   end
 
