e423fd12837e0600695f23b0ceebbf35a3692c15
Zachary Lehmann <zlehmann@gmail.com>
2019-02-03 13:45:55 +0000

    Automatically backed up by Learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -8,6 +8,11 @@ module SongsHelper
   end
 
   def display_artist(song)
+    if song.artist.nil?
+      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+    else
+      hidden_field_tag "song[artist_id]", song.artist_id
+    end
   end
 
   def artist_select(one, two)
